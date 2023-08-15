49a603e340353e010dc9744a2cc9c3179f949646
DevEsterCarvalho <estercarvalho205@gmail.com>
2023-08-15 12:13:05 -0300

    subindo exercicio de leitura de dados

--- src/br/com/fiap/tds/IfElse.java
+++ src/br/com/fiap/tds/IfElse.java
@@ -1,5 +1,17 @@
 package br.com.fiap.tds;
 
 public class IfElse {
+	public static void main(String[] args) {
+	int x= 15;
+		
+	if(x > 10){
+		System.out.println("X é maior do que 10");
+	} else if(x == 10) {
+		System.out.println("X é igual a 10");
+	} else {
+		System.out.println("X é menor do que 10");
+	}
+	
+	}
 
 }
